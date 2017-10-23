9304b0ec842b3ca38a72802aa5191487038b9c06
morenod <dmorenocuellar@gmail.com>
2017-10-23 11:22:54 -0400

    added pods and talking to firebase storage now

--- mindful/mindful/CameraController.swift
+++ mindful/mindful/CameraController.swift
@@ -10,6 +10,7 @@ import AVFoundation
 import UIKit
 import Vision
 import Photos
+import FirebaseStorage
 
 class CameraController : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureFileOutputRecordingDelegate {
     var captureSession : AVCaptureSession?
@@ -41,7 +42,6 @@ extension CameraController {
         case noCamerasAvailable
         case unknown
     }
-    
     public enum CameraPosition {
         case front
         case rear
@@ -71,39 +71,42 @@ extension CameraController {
         }
         func configureDeviceInputs() throws {
             guard let captureSession = self.captureSession else { throw CameraControllerError.captureSessionIsMissing }
-            
             if let rearCamera = self.rearCamera {
                 self.rearCameraInput = try AVCaptureDeviceInput(device: rearCamera)
-                
                 if captureSession.canAddInput(self.rearCameraInput!) { captureSession.addInput(self.rearCameraInput!) }
-                
                 self.currentCameraPosition = .rear
-            }
-                
-            else if let frontCamera = self.frontCamera {
+            } else if let frontCamera = self.frontCamera {
                 self.frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
                 
                 if captureSession.canAddInput(self.frontCameraInput!) { captureSession.addInput(self.frontCameraInput!) }
                 else { throw CameraControllerError.inputsAreInvalid }
                 
                 self.currentCameraPosition = .front
-            }
+            } else { throw CameraControllerError.noCamerasAvailable }
+            do {
+                let audioDevice = AVCaptureDevice.default(for: .audio)
+                let audioDeviceInput = try AVCaptureDeviceInput(device: audioDevice!)
                 
-            else { throw CameraControllerError.noCamerasAvailable }
+                if captureSession.canAddInput(audioDeviceInput) {
+                    captureSession.addInput(audioDeviceInput)
+                } else {
+                    print("Could not add audio device input to the session")
+                }
+            } catch {
+                print("Could not create audio device input: \(error)")
+            }
         }
         func configurePhotoOutput() throws {
             guard let captureSession = self.captureSession else { throw CameraControllerError.captureSessionIsMissing }
-            
             self.videoOutput = AVCaptureVideoDataOutput()
             self.audioOutput = AVCaptureAudioDataOutput()
             let settings: [AnyHashable: Any] = [kCVPixelBufferPixelFormatTypeKey as AnyHashable: Int(kCVPixelFormatType_32BGRA)]
             self.videoOutput?.videoSettings = settings as! [String : Any]
             self.videoOutput?.setSampleBufferDelegate(self, queue: facesQueue)
             if captureSession.canAddOutput(self.videoOutput!) { captureSession.addOutput(self.videoOutput!) }
-//            captureSession.addOutput(self.audioOutput!)
+            if captureSession.canAddOutput(self.audioOutput!) { captureSession.addOutput(self.audioOutput!) }
             captureSession.startRunning()
         }
-        
         DispatchQueue(label : "prepare").async {
             do {
                 createCaptureSession()
@@ -137,42 +140,28 @@ extension CameraController {
         self.previewLayer?.frame = view.frame
         self.landmarksLayer?.frame = view.frame
     }
-    
     func switchCameras() throws {
         guard let currentCameraPosition = currentCameraPosition, let captureSession = self.captureSession, captureSession.isRunning else {throw CameraControllerError.captureSessionIsMissing}
         func switchToFrontCamera() throws {
             guard let inputs = captureSession.inputs as? [AVCaptureInput], let rearCameraInput = self.rearCameraInput, inputs.contains(rearCameraInput),
-                let frontCamera = self.frontCamera else { throw CameraControllerError.invalidOperation }
-            
+            let frontCamera = self.frontCamera else { throw CameraControllerError.invalidOperation }
             self.frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
-            
             captureSession.removeInput(rearCameraInput)
-            
             if captureSession.canAddInput(self.frontCameraInput!) {
                 captureSession.addInput(self.frontCameraInput!)
-                
                 self.currentCameraPosition = .front
-            }
-                
-            else { throw CameraControllerError.invalidOperation }
+            } else { throw CameraControllerError.invalidOperation }
         }
         func switchToRearCamera() throws {
             guard let inputs = captureSession.inputs as? [AVCaptureInput], let frontCameraInput = self.frontCameraInput, inputs.contains(frontCameraInput),
-                let rearCamera = self.rearCamera else { throw CameraControllerError.invalidOperation }
-            
+            let rearCamera = self.rearCamera else { throw CameraControllerError.invalidOperation }
             self.rearCameraInput = try AVCaptureDeviceInput(device: rearCamera)
-            
             captureSession.removeInput(frontCameraInput)
-            
             if captureSession.canAddInput(self.rearCameraInput!) {
                 captureSession.addInput(self.rearCameraInput!)
-                
                 self.currentCameraPosition = .rear
-            }
-                
-            else { throw CameraControllerError.invalidOperation }
+            } else { throw CameraControllerError.invalidOperation }
         }
-        
         switch currentCameraPosition {
         case.front :
             try switchToRearCamera()
@@ -181,53 +170,74 @@ extension CameraController {
         }
         captureSession.commitConfiguration()
     }
-    func randomVideoFileName(length: Int) -> String {
-        
-        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        let len = UInt32(letters.length)
-        
-        var randomString = ""
-        
-        for _ in 0 ..< length {
-            let rand = arc4random_uniform(len)
-            var nextChar = letters.character(at: Int(rand))
-            randomString += NSString(characters: &nextChar, length: 1) as String
-        }
-        randomString += ".mov"
-        
-        return randomString
-    }
-    
-    
     func beginRecording () throws {
         self.captureSession?.removeOutput(self.videoOutput!)
         self.captureSession?.addOutput(self.videoFileOutput)
-        let exportPath = NSTemporaryDirectory().appendingFormat(randomVideoFileName(length: 8))
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone.current
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = formatter.string(from: now)+".mov"
+        let exportPath = NSTemporaryDirectory().appendingFormat(dateString)
         self.exportURL = URL(fileURLWithPath: exportPath)
         self.videoFileOutput.startRecording(to: self.exportURL!, recordingDelegate: self.recordingDelegate!)
     }
     
     func stopRecording() throws {
         self.videoFileOutput.stopRecording()
-        
-        PHPhotoLibrary.shared().performChanges({
-            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: self.exportURL!)
-        }) { saved, error in
-            if saved {
-               print("Saved the video")
-            } else if (error != nil) {
-                print(error!.localizedDescription)
-            }
-        }
         self.captureSession?.removeOutput(self.videoFileOutput)
         self.captureSession?.addOutput(self.videoOutput!)
     }
-    
     func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
         return
     }
-    
     func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let videoRef = storageRef.child("videos/"+outputFileURL.lastPathComponent)
+        let metadata = StorageMetadata()
+        metadata.contentType = "video/mov"
+        let uploadTask = videoRef.putFile(from: outputFileURL, metadata: metadata)
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.observe(.resume) { snapshot in
+            // Upload resumed, also fires when the upload starts
+        }
+        
+        uploadTask.observe(.pause) { snapshot in
+            // Upload paused
+        }
+        
+        uploadTask.observe(.progress) { snapshot in
+            // Upload reported progress
+            let percentComplete = 100.0 * Double(snapshot.progress!.completedUnitCount)
+                / Double(snapshot.progress!.totalUnitCount)
+        }
+        uploadTask.observe(.success) { snapshot in
+            // Upload completed successfully
+        }
+        uploadTask.observe(.failure) { snapshot in
+            if let error = snapshot.error as? NSError {
+                switch (StorageErrorCode(rawValue: error.code)!) {
+                case .objectNotFound:
+                    // File doesn't exist
+                    break
+                case .unauthorized:
+                    // User doesn't have permission to access file
+                    break
+                case .cancelled:
+                    // User canceled the upload
+                    break
+                case .unknown:
+                    // Unknown error occurred, inspect the server response
+                    print("unknown")
+
+                    break
+                default:
+                    // A separate error occurred. This is a good place to retry the upload.
+                    break
+                }
+            }
+        }
         return
     }
     
@@ -241,7 +251,6 @@ extension CameraController {
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
-        
         let attachments = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate)
         let ciImage = CIImage(cvImageBuffer: pixelBuffer!, options: attachments as! [String : Any]?)
         let ciImageWithOrientation = ciImage.oriented(forExifOrientation: Int32(UIImageOrientation.leftMirrored.rawValue))
@@ -310,10 +319,8 @@ extension CameraController {
             let faceLandmarkPoints = points.map { (point: CGPoint) -> (x: CGFloat, y: CGFloat) in
                 let pointX = point.x * boundingBox.width + boundingBox.origin.x
                 let pointY = point.y * boundingBox.height + boundingBox.origin.y
-                
                 return (x: pointX, y: pointY)
             }
-            
             DispatchQueue.main.async {
                 self.draw(points: faceLandmarkPoints)
             }
