dd88daf0d74d5b2e4fcd178f6bf90b69530bb162
Anvita Achar <anvitaachar@vagelos-ve509-0718.apn.wlan.upenn.edu>
2018-02-22 19:01:22 -0500

    checks for auth within appdelegate

--- mindful/mindful/AppDelegate.swift
+++ mindful/mindful/AppDelegate.swift
@@ -14,24 +14,33 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    
+    
+    
+    
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-      
-       
-        
+        let auth = Auth.auth()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let loginViewController = MainViewController()
-     
-//        let homeViewController = ViewController()
-//        window!.rootViewController = homeViewController
-//        window!.makeKeyAndVisible()
-        window!.rootViewController = loginViewController
+        
+        if auth.currentUser != nil {
+            let homeViewController = ViewController()
+            window!.rootViewController = homeViewController
+            
+        } else {
+            let loginViewController = MainViewController()
+            window!.rootViewController = loginViewController
+        }
+        
+        
+       
+        
         window!.makeKeyAndVisible()
         return true
         
--- mindful/mindful/MainViewController.swift
+++ mindful/mindful/MainViewController.swift
@@ -23,7 +23,8 @@ class MainViewController: UIViewController, FUIAuthDelegate {
         self.auth = Auth.auth()
         self.authUI = FUIAuth.defaultAuthUI()
         self.authUI?.delegate = self
-       
+        
+
         
         self.authStateListenerHandle = self.auth?.addStateDidChangeListener { (auth, user) in
             guard user != nil else {
@@ -50,11 +51,9 @@ class MainViewController: UIViewController, FUIAuthDelegate {
         guard let authError = error else {
             
             let x = authDataResult?.user.email
-            print("siging")
-            print(x!)
             var mainAppController: ViewController? = nil
             mainAppController = ViewController()
-            self.show(mainAppController!, sender: nil);
+            self.show(mainAppController!, sender: nil)
             
             return
         }
